bbda60419dad1d2543e6cca80ece56562145c82a
Elanchezhiyan M <chezhiyanmahi2020@gmail.com>
2024-11-13 15:28:52 +0530

    Home page created

--- halal/src/App.tsx
+++ halal/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Button } from './components/ui/button'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <div>
-      <Button>Click me</Button>
+    <Home/>
     </div>
   )
 }
