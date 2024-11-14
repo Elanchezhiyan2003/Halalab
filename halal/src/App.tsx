e04b70011fbf9c73d0727591252c691bf42f7872
Elanchezhiyan M <chezhiyanmahi2020@gmail.com>
2024-11-14 14:42:12 +0530

    Clear unwanted sources

--- halal/src/App.tsx
+++ halal/src/App.tsx
@@ -4,9 +4,9 @@ import { Button } from './components/ui/button'
 const App = () => {
   return (
     <div>
-      <Button>Click me</Button>
+      
     </div>
   )
 }
 
-export default App
+export default App;
