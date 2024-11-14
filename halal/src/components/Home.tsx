5ef5bd08702972e44c55fc7a7e785905f67d44a6
Elanchezhiyan M <chezhiyanmahi2020@gmail.com>
2024-11-14 12:41:33 +0530

    Change font sizes

--- halal/src/components/Home.tsx
+++ halal/src/components/Home.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 import { Button } from './ui/button';
 import homeimg from '@/assets/home.png'
 
 const home = () => {
   return (
-    <div className="h-96 w-full flex items-center bg-blue-600">
+<div className="h-96 w-full flex items-center bg-blue-600">
     <div className="w-9/12 p-8 mx-5 flex flex-col justify-center max-w-md">
         <h1 className="text-white text-5xl font-bold mb-4">Start with your identity</h1>
-        <p className="text-white text-lg mb-6">
+        <p className="text-white font-medium mb-6">
             With Identity, you can improve customer sign-ups, employee productivity, and get apps to market faster.
         </p>
         <div className="flex gap-4">
@@ -16,11 +16,9 @@ const home = () => {
         </div>
     </div>
     <div className="flex-1 h-full">
-        <img className="h-80 mx-60 my-2 rounded-lg" src={homeimg} alt="Identity Solution" />
+        <img className="h-80 mx-60 my-8 border-white border-4  rounded-lg" src={homeimg} alt="Identity Solution"/>
     </div>
 </div>
-
   )
 }
-
 export default home;
