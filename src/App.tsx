a223231fc332aa7a24cfbcc3f01495ebcb70d61d
hemakarthikeyan2003@gmail.com <hemakarthikeyan2003@gmail.com>
2024-11-15 14:52:41 +0530

    new files 1 received

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+import Footer from './components/footer'
+import Header from './components/header'
+
+import Home from './components/Home'
+
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App;
